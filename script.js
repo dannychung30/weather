b40e84c2b94a73bdc46db87425e940b2ae7200d7
Danny Chung <dannychung6158@gmail.com>
2024-09-01 13:06:08 -0700

    grabbed data from form input to display weather info into console

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
+// Get global variables
 const apiKey = "c29d4cec29b559dd93b3da4906fbf049";
 
 document.querySelector("form").addEventListener("submit", function(e) {
     e.preventDefault();
+
+    const city = document.querySelector(".weather-form .city-input").value;
+    const state = document.querySelector(".weather-form .state-input").value;
+
+    getCoordinates(city, state);
+
 });
 
 async function getCoordinates(city, state) {
-    
-    const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},840&limit=1&appid=${apiKey}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
-}
 
-async function getWeather() {
-
-    const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-    const response = await fetch(apiUrl);
+    const coordinatesApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},840&limit=1&appid=${apiKey}`;
+    const response = await fetch(coordinatesApiUrl);
     const data = await response.json();
 
-    console.log(data);
+    getWeather(data[0].lat, data[0].lon);
 }
 
-// getCoordinates();
-// getWeather();
+async function getWeather(latitude, longitude) {
+
+    const weatherApiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+    const response = await fetch(weatherApiUrl);
+    const data = await response.json();
+
+    console.log(data.current);
+}
